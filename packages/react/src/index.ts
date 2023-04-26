97f96470568645c2358677a9c8551e0d541fd84d
Kaifaty <andry.diego2011@gmail.com>
2023-04-27 00:38:51 +0500

    fix

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,6 @@
 import {useSyncExternalStore} from 'react'
 import type {Common, StateType} from '@statx/core'
 
-export const useState = <T extends StateType>(state: Common<T>) => {
+export const useStatx = <T extends StateType>(state: Common<T>) => {
   return useSyncExternalStore(state.subscribe, state)
 }
