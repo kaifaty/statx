2708c6fbd6154f6fbd4b8ef5710afe1a07f481dd
Kaifaty <andry.diego2011@gmail.com>
2024-10-27 11:57:51 +0300

    update

--- packages/lit/src/XLitElement.ts
+++ packages/lit/src/XLitElement.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import type {PropertyValues} from 'lit'
 import {LitElement} from 'lit'
 import {recorder} from '@statx/core'
 import {isEqualSet} from '@statx/utils'
@@ -8,50 +9,40 @@ import type {CommonInternal, UnSubscribe} from '@statx/core'
 export type Constructor<T> = new (...args: any[]) => T
 type Subs = Set<CommonInternal>
 
-interface BaseUpdatedElement extends HTMLElement {
-  updated(..._args: any[]): void
-  requestUpdate(..._args: any[]): void
-  willUpdate(..._args: any[]): void
-}
+export class XLitElement extends LitElement {
+  private _subs: UnSubscribe[] = []
+  private _prevSnapshot?: Subs
+  private _unsubAll() {
+    this._subs.forEach((unsub) => unsub())
+    this._subs.length = 0
+  }
+  private _updater = () => this.requestUpdate()
+  connectedCallback() {
+    //@ts-ignore
+    super.connectedCallback?.()
+  }
 
-export const statable = <T extends Constructor<BaseUpdatedElement>>(superClass: T): T => {
-  return class StatableLit extends superClass {
-    private _subs: UnSubscribe[] = []
-    private _prevSnapshot?: Subs
-    private _unsubAll() {
-      this._subs.forEach((unsub) => unsub())
-      this._subs.length = 0
-    }
-    private _updater = () => this.requestUpdate()
-    connectedCallback() {
-      //@ts-ignore
-      super.connectedCallback?.()
-    }
+  updated(_changedProperties: PropertyValues): void {
+    super.updated(_changedProperties)
+    const data = recorder.flush()
 
-    updated(...args: any[]): void {
-      super.updated(args)
-      const data = recorder.flush()
+    if (data && isEqualSet(data, this._prevSnapshot)) {
+      return
+    }
+    this._prevSnapshot = data
+    this._unsubAll()
 
-      if (data && isEqualSet(data, this._prevSnapshot)) {
-        return
-      }
-      this._prevSnapshot = data
-      this._unsubAll()
-
-      data?.forEach((state) => {
-        this._subs.push(state.subscribe(this._updater))
-      })
-    }
-    willUpdate(...args: any[]): void {
-      super.willUpdate(args)
-      recorder.start()
-    }
-    disconnectedCallback(): void {
-      //@ts-ignore
-      super.disconnectedCallback?.()
-      this._unsubAll()
-    }
+    data?.forEach((state) => {
+      this._subs.push(state.subscribe(this._updater))
+    })
+  }
+  willUpdate(_changedProperties: PropertyValues): void {
+    super.willUpdate(_changedProperties)
+    recorder.start()
+  }
+  disconnectedCallback(): void {
+    //@ts-ignore
+    super.disconnectedCallback?.()
+    this._unsubAll()
   }
 }
-
-export const XLitElement = statable(LitElement as any)
